6f5a62152bdb239f04e20f98b84497f2e9e297c5
AIGLEKIVOL <theo.dorget@gmail.com>
2022-09-06 11:33:10 +0200

    Essai push package tests

---
+++ Polluxia/src/pollux/tests/TestMediumMotor.java
@@ -0,0 +1,14 @@
+package pollux.tests;
+
+public class TestMediumMotor {
+
+	public TestMediumMotor() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
